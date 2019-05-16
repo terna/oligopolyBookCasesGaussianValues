bcde3ae79b75ad87c85e306de2705fa9daac3fe7
Pietro Terna <pietro.terna@unito.it>
2019-05-16 11:21:40 +0200

    add readOnline.py

---
+++ readOnline.py
@@ -0,0 +1,20 @@
+# https://raw.githubusercontent.com/terna/oligopoly/master/exampleGauss/11.txt
+
+
+# https://stackoverflow.com/questions/15138614/how-can-i-read-the-contents-of-an-url-with-python
+
+
+import urllib.request
+import os
+
+link = "https://raw.githubusercontent.com/terna/oligopolyBookCasesGaussianValues/master/11.txt"
+
+try:
+    f = urllib.request.urlopen(link)
+except:
+    print("online data not found")
+    os.sys.exit(1)
+
+for i in range(10):
+    d = f.readline()  
+    print(float(d))
